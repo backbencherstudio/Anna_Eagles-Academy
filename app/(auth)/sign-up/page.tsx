bc10397f65c5c87155b389175effabef394673ff
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-07-22 17:09:23 +0600

    login and signup page design

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import SignUp from '../_components/SignUp'
 
-export default function SignUp() {
+export default function SignUpPage() {
     return (
-        <div>
-            this is sign up
-        </div>
+        <SignUp />
     )
 }
