401031f9ae29dcf04f82db00823646dd1d5c7590
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-07-29 17:10:30 +0600

    adde report card data

---
+++ app/(dashboard)/(admin)/reports/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FinancialReportsCard from '../../_components/Admin/FinancialReportsCard'
+import TableTransactions from '../../_components/Admin/TableTransactions'
+
+export default function ReportsPage() {
+    return (
+        <div className='flex flex-col gap-4 bg-white rounded-lg p-4 border border-gray-100'>
+            <FinancialReportsCard />
+            <TableTransactions />
+        </div>
+    )
+}
---
+++ app/(dashboard)/_components/Admin/TableTransactions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TableTransactions() {
+    return (
+        <div>
+            this is table transactions
+        </div>
+    )
+}
